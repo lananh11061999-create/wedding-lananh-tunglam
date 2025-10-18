34fc82ad9809cc169e26090a42dadde33bc10449
Lan Anh <lananh11061999@gmail.com>
2025-10-18 22:13:51 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,9 @@ export default function Home(){
       <Venue />
       <Gallery />
       <RSVP />
+            <p style={{maxWidth:800, margin:'18px auto 0 auto', textAlign:'center', color:'#5a4040'}}>
+         Chúng mình sẽ hạnh phúc hơn khi  dành thời gian đến chứng kiến khoảnh khắc tụi mình chính thức về chung một nhà.
+        </p>
       <footer>© Lan Anh & Tùng Lâm — 28/12/2025</footer>
     </div>
   )
