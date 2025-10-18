ad738c30ad827103ebc779e6b124483f10e04130
Lan Anh <lananh11061999@gmail.com>
2025-10-18 21:19:38 +0700

    Update Story.js

--- components/Story.js
+++ components/Story.js
@@ -4,8 +4,8 @@ export default function Story(){
       <div className="container">
         <h2 style={{textAlign:'center', color:'var(--main-1)'}}>Câu chuyện của chúng tôi</h2>
         <p style={{maxWidth:800, margin:'18px auto 0 auto', textAlign:'center', color:'#5a4040'}}>
-          Từ lần gặp đầu tiên đến hôm nay là một hành trình nhẹ nhàng, tràn ngập tiếng cười và những kỷ niệm ấm áp.
-          Chúng tôi rất mong được chia sẻ ngày vui cùng bạn.
+          Hai năm yêu xa, chúng mình đã cùng đi qua biết bao nhớ thương, mong chờ và cả những ngày đếm ngược để được ở bên nhau. 
+            Hôm nay, khi khoảng cách không còn nữa, chúng mình hạnh phúc mời bạn cùng chung vui trong ngày đặc biệt – ngày tình yêu được kết trái bằng một đám cưới trọn vẹn.
         </p>
       </div>
     </section>
